3ff01d009725d4574620203d60101d88e3ef91bb
Roy <shuwj81@daum.net>
2025-02-25 22:06:20 +0900

    ♻️[refactor]: RegisterModule에 UseCaseModuleFactory, RepositoryModuleFactory 에서 쓰던 함수 이동

--- Attendance/Projects/Core/Networking/DiContainer/Sources/RegisterModule.swift
+++ Attendance/Projects/Core/Networking/DiContainer/Sources/RegisterModule.swift
@@ -13,4 +13,55 @@ public struct RegisterModule {
   public func makeModule<T>(_ type: T.Type, factory: @escaping () -> T) -> Module {
     Module(type, factory: factory)
   }
+  
+  // MARK: - Repository 생성
+  public func makeRepository<T, U>(
+    _ protocolType: T.Type, factory: @escaping () -> U
+  ) -> () -> Module {
+    return {
+      self.makeRepositoryModule(protocolType) {
+        guard let repository = factory() as? T else {
+          fatalError("Failed to cast \(U.self) to \(T.self)")
+        }
+        return repository
+      }
+    }
+  }
+  
+  // MARK: - RepositoryModule 생성
+  private func makeRepositoryModule<T>(
+    _ type: T.Type,
+    factory: @escaping () -> T
+  ) -> Module {
+    self.makeModule(type, factory: factory)
+  }
+  
+  // MARK: -  usecase 생성
+  public func makeUseCase<T, U>(_ protocolType: T.Type, factory: @escaping () -> U) -> () -> Module {
+    return {
+      self.makeUseCaseModule(protocolType) {
+        guard let useCase = factory() as? T else {
+          fatalError("Failed to cast \(U.self) to \(T.self)")
+        }
+        return useCase
+      }
+    }
+  }
+  
+  
+  // MARK: -  usecase 모듈 생성
+  private func makeUseCaseModule<T>(
+    _ type: T.Type,
+    factory: @escaping () -> T
+  ) -> Module {
+    self.makeModule(type, factory: factory)
+  }
+  
+  // MARK: -  di에 등록
+  public func resolveOrDefault<T>(
+    _ type: T.Type,
+    default defaultFactory: @autoclosure @escaping () -> T
+  ) -> T {
+    DependencyContainer.live.resolveOrDefault(type, default: defaultFactory())
+  }
 }
