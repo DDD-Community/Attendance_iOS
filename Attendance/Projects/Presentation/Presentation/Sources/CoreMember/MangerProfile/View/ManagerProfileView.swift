4ad34508d5cfa18dc638d28484abdddb72c4325b
Roy <shuwj81@daum.net>
2025-03-23 00:57:30 +0900

    ✨[feat]:  운영진 프로필 화면구현 #45

---
+++ Attendance/Projects/Presentation/Presentation/Sources/CoreMember/MangerProfile/View/ManagerProfileView.swift
@@ -0,0 +1,257 @@
+//
+//  ManagerProfileView.swift
+//  DDDAttendance
+//
+//  Created by 서원지 on 7/17/24.
+//
+
+import SwiftUI
+
+import Model
+
+import ComposableArchitecture
+import SDWebImageSwiftUI
+
+public struct ManagerProfileView: View {
+  @Bindable private var store: StoreOf<ManagerProfile>
+  private var backAction: () -> Void
+  
+  public init(
+    store: StoreOf<ManagerProfile>,
+    backAction: @escaping () -> Void
+  ) {
+    self.store = store
+    self.backAction = backAction
+  }
+  
+  public var body: some View {
+    ZStack {
+      Color.basicBlack
+        .edgesIgnoringSafeArea(.all)
+      
+      VStack {
+        mangerProfileLoadingData()
+      }
+      .task {
+        store.send(.async(.fetchUser))
+      }
+    }
+  }
+}
+
+extension ManagerProfileView {
+  
+  @ViewBuilder
+  fileprivate func mangerProfileLoadingData() -> some View {
+    if store.userMember?.name ==  nil {
+      if store.isLoading  {
+        profileLoadingView()
+      }
+  } else {
+    mangerProfileData()
+  }
+}
+  
+  @ViewBuilder
+  fileprivate func mangerProfileData() -> some View {
+    VStack {
+      Spacer()
+        .frame(height: 16)
+      
+      CustomNavigationBackBar(buttonAction: backAction)
+      
+      mangerCardImage()
+      
+      logoutButton()
+    }
+  }
+  
+  
+  @ViewBuilder
+  fileprivate func mangerCardImage() -> some View {
+    LazyVStack {
+      Spacer()
+        .frame(height: 17)
+      
+      VStack(alignment: .leading, spacing: .zero) {
+        Spacer()
+          .frame(height: 24)
+        
+        HStack {
+          Text("운영진")
+            .pretendardCustomFont(textStyle: .body3NormalBold)
+            .foregroundStyle(.statusFocus)
+            .padding(.horizontal, 14)
+            .padding(.vertical, 5)
+            .overlay {
+              RoundedRectangle(cornerRadius: 20)
+                .stroke(.statusFocus, lineWidth: 1)
+                .background(.clear)
+            }
+          
+          Spacer()
+        }
+        
+        Spacer()
+          .frame(height: 8)
+        
+        Text("\(store.userMember?.name ?? "")님")
+          .pretendardCustomFont(textStyle: .headline5Bold)
+          .foregroundStyle(.borderInverse)
+        
+        Spacer()
+        
+        managerTextComponent(
+          title: store.managerProfileRoleType,
+          subTitle: store.userMember?.role.attendanceListDesc ?? "",
+          managingTeam: "",
+          isManaging: false,
+          isGeneration: false
+        )
+        
+        Spacer()
+          .frame(height: 20)
+        
+        if store.userMember?.managing == .projectTeamManaging {
+          managerTextComponent(
+            title: store.managerProfileManaging,
+            subTitle: store.userMember?.managing.managingDesc ?? "",
+            managingTeam: store.userMember?.memberTeam?.attendanceListDescription ?? "",
+            isManaging: false,
+            isGeneration: false
+          )
+        } else {
+          managerTextComponent(
+            title: store.managerProfileManaging,
+            subTitle: store.userMember?.managing.managingDesc ?? "",
+            managingTeam: "",
+            isManaging: false,
+            isGeneration: false
+          )
+        }
+        
+        Spacer()
+          .frame(height: 20)
+        
+        managerTextComponent(
+          title: store.managerProfileGeneration,
+          subTitle: store.userMember?.generation.description ?? "",
+          managingTeam: "",
+          isManaging: false,
+          isGeneration: true
+        )
+        
+        Spacer()
+          .frame(height: 40)
+        
+        HStack {
+          Spacer()
+          
+          Text("Dynamic Developer Designers")
+            .pretendardCustomFont(textStyle: .body3NormalMedium)
+            .foregroundStyle(.textSecondary100)
+          
+          Spacer()
+        }
+        
+        Spacer()
+          .frame(height: 24)
+        
+      }
+      .padding(.horizontal, 24)
+      .background(
+        Image(asset: .profileBack)
+          .resizable()
+          .scaledToFit()
+          .frame(height: UIScreen.screenHeight * 0.7)
+          .cornerRadius(20)
+      )
+      .frame(height: UIScreen.screenHeight * 0.7)
+    }
+    .padding(.horizontal, 24)
+  }
+  
+  @ViewBuilder
+  private func managerTextComponent(
+    title: String,
+    subTitle: String,
+    managingTeam: String,
+    isManaging: Bool,
+    isGeneration: Bool
+  ) -> some View {
+    LazyVStack(spacing: .zero) {
+      HStack {
+        Text(title)
+          .pretendardCustomFont(textStyle: .body2NormalMedium)
+          .foregroundStyle(.textSecondary100)
+        
+        Spacer()
+      }
+      
+      Spacer()
+        .frame(height: 2)
+      
+      if isManaging {
+        HStack {
+          Text("\(subTitle) / \(managingTeam)팀")
+            .pretendardCustomFont(textStyle: .title2NormalBold)
+            .foregroundStyle(.borderInverse)
+          
+          Spacer()
+        }
+      } else if isGeneration {
+        HStack {
+          Text("\(subTitle)기")
+            .pretendardCustomFont(textStyle: .title2NormalBold)
+            .foregroundStyle(.borderInverse)
+          
+          Spacer()
+        }
+      } else {
+        HStack {
+          Text(subTitle)
+            .pretendardCustomFont(textStyle: .tilte1NormalBold)
+            .foregroundStyle(.borderInverse)
+          
+          Spacer()
+        }
+      }
+    }
+    .padding(.horizontal, 24)
+  }
+  
+  @ViewBuilder
+  fileprivate func profileLoadingView() -> some View {
+    VStack {
+      Spacer()
+      
+      AnimatedImage(name: "DDDLoding.gif", isAnimating: .constant(true))
+        .resizable()
+        .scaledToFit()
+        .frame(width: 200, height: 200)
+      
+      Spacer()
+    }
+  }
+  
+  @ViewBuilder
+  private func logoutButton() -> some View {
+    VStack {
+      Spacer()
+      
+      HStack(alignment: .center) {
+        Text(store.logoutText)
+          .pretendardFont(family: .Regular, size: 16)
+          .foregroundStyle(.gray300)
+          .underline(true, color: Color.gray300)
+      }
+      .onTapGesture {
+        store.send(.navigation(.presentLogOut))
+      }
+      
+      Spacer()
+        .frame(height: 24)
+    }
+    .padding(.horizontal, 24)
+  }
+}
