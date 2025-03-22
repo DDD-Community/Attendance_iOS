09d93b3c3cec28170ce71ab867b1ba9c69541c00
Roy <shuwj81@daum.net>
2025-03-22 20:32:40 +0900

    ✨[feat]: DI에 빠진 repository 추가  #44
    * Di Thread Safe 하게 변경

---
+++ Attendance/Projects/App/Sources/Di/RepositoryModuleFactory.swift
@@ -0,0 +1,26 @@
+//
+//  RepositoryModuleFactory.swift
+//  DDDAttendance
+//
+//  Created by Wonji Suh  on 12/2/24.
+//
+
+import Foundation
+
+import DiContainer
+import Networkings
+
+extension RepositoryModuleFactory {
+  public mutating func registerDefaultDefinitions() {
+    let registerModuleCopy = registerModule  // self를 직접 캡처하지 않고 복사
+    repositoryDefinitions = {
+      return [
+        registerModuleCopy.makeDependency(AuthRepositoryProtocol.self) { AuthRepository() },
+        registerModuleCopy.makeDependency(OAuthRepositoryProtocol.self) { OAuthRepository() },
+        registerModuleCopy.makeDependency(FireStoreRepositoryProtocol.self) { FireStoreRepository() },
+        registerModuleCopy.makeDependency(QrCodeRepositoryProtcol.self) { QrCodeRepository() },
+        registerModuleCopy.makeDependency(SignUpRepositoryProtcol.self) { SignUpRepository() }
+      ]
+    }()
+  }
+}
