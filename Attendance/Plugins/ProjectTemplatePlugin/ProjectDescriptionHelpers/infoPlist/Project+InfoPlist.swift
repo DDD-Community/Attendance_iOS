6b854de560de7aa1a09d54f5ab422ed07152c83c
Roy <shuwj81@daum.net>
2025-03-22 20:31:19 +0900

    ✨[feat]: tuist 설정 변경 #44
    * Config 추가
    * infoplist 선언형으로 변경

---
+++ Attendance/Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/infoPlist/Project+InfoPlist.swift
@@ -0,0 +1,60 @@
+//
+//  Project+InfoPlist.swift
+//  Plugins
+//
+//  Created by Wonji Suh  on 3/22/25.
+//
+
+import Foundation
+import ProjectDescription
+
+public extension InfoPlist {
+  static let appInfoPlist: Self = .extendingDefault(
+    with: InfoPlistDictionary()
+      .setUIUserInterfaceStyle("Light")
+      .setCFBundleDevelopmentRegion("$(DEVELOPMENT_LANGUAGE)")
+      .setCFBundleExecutable("$(EXECUTABLE_NAME)")
+      .setCFBundleIdentifier("$(PRODUCT_BUNDLE_IDENTIFIER)")
+      .setCFBundleInfoDictionaryVersion("6.0")
+      .setCFBundleName("DDD 출석")
+      .setCFBundlePackageType("APPL")
+      .setCFBundleShortVersionString(.appVersion())
+      .setAppTransportSecurity()
+      .setCFBundleURLTypes()
+      .setAppUseExemptEncryption(value: false)
+      .setCFBundleVersion(.appBuildVersion())
+      .setLSRequiresIPhoneOS(true)
+      .setUIAppFonts(["PretendardVariable.ttf"])
+      .setUIApplicationSceneManifest([
+        "UIApplicationSupportsMultipleScenes": true,
+        "UISceneConfigurations": [
+          "UIWindowSceneSessionRoleApplication": [
+            [
+              "UISceneConfigurationName": "Default Configuration",
+              "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+            ]
+          ]
+        ]
+      ])
+      .setUIRequiredDeviceCapabilities(["armv7"])
+      .setUISupportedInterfaceOrientations(["UIInterfaceOrientationPortrait"])
+      .setNSCameraUsageDescription("QR 코드 인식을 위해 카메라 접근 권한이 필요합니다")
+      .setUILaunchScreens()
+      .setCalenderUsage("캘린더의 정보를 가져오기 위해서 접근권할을 허용해주세요")
+      .setCFBundleDevelopmentRegion()
+      .setGoogleReversedClientID("${REVERSED_CLIENT_ID}")
+      .setGoogleClientID("${GOOGLE_CLIENT_ID}")
+      .setGIDClientID("${GOOGLE_CLIENT_ID}")
+  )
+  
+  static let moduleInfoPlist: Self = .extendingDefault(
+    with: InfoPlistDictionary()
+      .setUIUserInterfaceStyle("Light")
+      .setCFBundleDevelopmentRegion("$(DEVELOPMENT_LANGUAGE)")
+      .setCFBundleExecutable("$(EXECUTABLE_NAME)")
+      .setCFBundleIdentifier("$(PRODUCT_BUNDLE_IDENTIFIER)")
+      .setCFBundleInfoDictionaryVersion("6.0")
+      .setCFBundlePackageType("APPL")
+      .setCFBundleShortVersionString(.appVersion())
+  )
+}
