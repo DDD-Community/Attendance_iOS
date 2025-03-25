56d4f2bff1ea93f2a49c472c91190824b9dbaac5
Roy <shuwj81@daum.net>
2025-03-25 22:39:15 +0900

    ✨[feat]:  달련 선택 관련 화면 추가  #44
    * 달력 수정
    * 라이브러리 추가로 인하여 diContainer 모듈 제거

--- Projects/Core/Networking/UseCase/Project.swift
+++ Projects/Core/Networking/UseCase/Project.swift
@@ -12,7 +12,6 @@ let project = Project.makeAppModule(
   dependencies: [
     .Networking(implements: .Model),
     .Networking(implements: .ThirdPartys),
-    .Networking(implements: .Service),
     .SPM.diContainer,
     .SPM.composableArchitecture,
     .SPM.googleSignIn,
