dbdf4733db46b42792e844b7207074618f11376b
Roy <shuwj81@daum.net>
2025-04-06 18:45:05 +0900

    ✨[feat]:  프로필에서 유저 정보 화면구현  #45
    * 유저 정보 화면 멤버로 사용 할수 있게 구현
    * 만든 유저들 모달 구현

---
+++ Attendance/Projects/Presentation/Presentation/Sources/CreateApp/View/CreateAppView.swift
@@ -0,0 +1,131 @@
+//
+//  CreateAppView.swift
+//  Presentation
+//
+//  Created by Wonji Suh  on 4/6/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import SwiftUIX
+
+struct CreateAppView: View {
+  @Bindable private var store: StoreOf<CreateApp>
+  private var closeAction:() -> Void = { }
+  
+  init(
+    store: StoreOf<CreateApp>,
+    closeAction: @escaping () -> Void
+  ) {
+    self.store = store
+    self.closeAction = closeAction
+  }
+  
+  var body: some View {
+    ZStack {
+      Color.basicBlack
+        .edgesIgnoringSafeArea(.all)
+      
+      VStack {
+        createAppHeaderView()
+        
+        createAppFooterView()
+        
+        closeButton()
+        
+        Spacer()
+          .frame(height: 40)
+      }
+    }
+  }
+}
+
+extension CreateAppView {
+  
+  @ViewBuilder
+  fileprivate func createAppHeaderView() -> some View {
+    VStack(alignment: .center) {
+      Spacer()
+        .frame(height: 16)
+      
+      Text("만든 사람들")
+        .pretendardCustomFont(textStyle: .title2NormalBold)
+        .foregroundStyle(.staticWhite)
+      
+    }
+  }
+  
+  @ViewBuilder
+  fileprivate func createAppFooterView() -> some View {
+    VStack(spacing: .zero) {
+      createAppPartItem(
+        selectPart: .design,
+        creators: "강동길, 이지윤, 조재인"
+      )
+      
+      createAppPartItem(
+        selectPart: .pm,
+        creators: "홍영주"
+      )
+      
+      createAppPartItem(
+        selectPart: .iOS,
+        creators: "서원지, 홍은표"
+      )
+      
+      createAppPartItem(
+        selectPart: .android,
+        creators: "심은석, 이상훈"
+      )
+      
+      createAppPartItem(
+        selectPart: .server,
+        creators: "조승준"
+      )
+      
+    }
+  }
+  
+  @ViewBuilder
+  fileprivate func createAppPartItem(
+    selectPart: SelectPart,
+    creators: String
+  ) -> some View {
+    VStack(alignment: .center) {
+      Spacer()
+        .frame(height: 24)
+      
+      Text(selectPart.attendanceListDesc)
+        .pretendardCustomFont(textStyle: .body3NormalRegular)
+        .foregroundStyle(.staticWhite)
+      
+      Spacer()
+        .frame(height: 2)
+      
+      Text(creators)
+        .pretendardCustomFont(textStyle: .title3NormalMedium)
+        .foregroundStyle(.staticWhite)
+      
+    }
+    
+  }
+  
+  @ViewBuilder
+  fileprivate func closeButton() -> some View {
+    VStack {
+      Spacer()
+        .frame(height: 36)
+      
+      
+      CustomButton(
+        action: closeAction,
+        title: "닫기",
+        config: CustomButtonConfig.createDateButton(),
+        isEnable: true
+      )
+      .padding(.horizontal, 24)
+    }
+    
+  }
+}
