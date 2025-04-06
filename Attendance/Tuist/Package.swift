e081528895f126bdad6a36ba744cb14a4cfdb046
Roy <shuwj81@daum.net>
2025-04-06 18:53:44 +0900

    ✨[feat]:  라이브러리 업데이트 및  Tuist update #45

--- Attendance/Tuist/Package.swift
+++ Attendance/Tuist/Package.swift
@@ -25,19 +25,19 @@ let package = Package(
     .package(url: "https://github.com/ReactorKit/ReactorKit", from: "3.2.0"),
     .package(url: "https://github.com/devxoul/Then", from: "3.0.0"),
     
-    .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.27.0"),
+      .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.27.0"),
     .package(url: "https://github.com/google/GoogleSignIn-iOS", from: "7.1.0"),
     
-    .package(url: "https://github.com/EFPrefix/EFQRCode.git", exact: "6.2.1"),
+      .package(url: "https://github.com/EFPrefix/EFQRCode.git", exact: "6.2.1"),
     
     
-    .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.0.0"),
+      .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.0.0"),
     .package(url: "https://github.com/kaishin/Gifu.git", from: "3.4.0"),
     
-    .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
+      .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
     .package(url: "https://github.com/exyte/PopupView.git", from: "2.10.4"),
     
-    .package(url: "http://github.com/pointfreeco/swift-composable-architecture", from: "1.18.0"),
+      .package(url: "http://github.com/pointfreeco/swift-composable-architecture", from: "1.18.0"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras.git", from: "1.1.0"),
     .package(url: "https://github.com/apple/swift-collections.git", branch: "main"),
     .package(url: "https://github.com/johnpatrickmorgan/TCACoordinators.git", from: "0.10.0"),
@@ -48,3 +48,4 @@ let package = Package(
     
   ]
 )
+
