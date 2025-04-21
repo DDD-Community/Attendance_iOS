602714c261c6a7294652665b09604775d5929c20
honghoker <sinbein9@gmail.com>
2025-04-22 02:05:08 +0900

    ✨[feat]: 일정표 목록 컴포넌트 추가 #30

---
+++ Attendance/Projects/Shared/DesignSystem/Sources/UI/Cell/ScheduleCellStyle.swift
@@ -0,0 +1,30 @@
+//
+//  ScheduleCellStyle.swift
+//  DesignSystem
+//
+//  Created by eunpyo on 4/22/25.
+//
+
+import SwiftUI
+
+public struct ScheduleCellStyle {
+  public let backgroundColor: Color
+  public let stampImage: Image?
+  public let dashBorder: Bool
+  public let monthDayOpacity: Double
+  public let titleDescriptionOpacity: Double
+
+  public init(
+    backgroundColor: Color,
+    stampImage: Image?,
+    dashBorder: Bool,
+    monthDayOpacity: Double,
+    titleDescriptionOpacity: Double
+  ) {
+    self.backgroundColor = backgroundColor
+    self.stampImage = stampImage
+    self.dashBorder = dashBorder
+    self.monthDayOpacity = monthDayOpacity
+    self.titleDescriptionOpacity = titleDescriptionOpacity
+  }
+}
