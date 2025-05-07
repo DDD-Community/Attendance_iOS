a601c523cf58e3a120ab72b468337dda2a6d216e
Roy <shuwj81@daum.net>
2025-05-08 02:14:51 +0900

    ✨[feat]:  Base API 설정  #48
    * 기본  base api 및 회원강비 api 파일 추가 및 구현

---
+++ Attendance/Projects/Core/Networking/Foundations/Sources/APIHeader/APIHeaderManger.swift
@@ -0,0 +1,20 @@
+//
+//  APIHeaderManger.swift
+//  Foundations
+//
+//  Created by Wonji Suh  on 5/7/25.
+//
+
+import Foundation
+
+public struct APIHeaderManger {
+  public static let shared = APIHeaderManger()
+  
+  public init() {}
+  
+  let appPackageName: String = "-"
+  let contentType: String = "application/json"
+  let multipartContentType: String = "multipart/form-data"
+  let contentAppleType: String = "application/x-www-form-urlencoded"
+  let csrf: String = "rXHHaia6Liv46KrAA8V1h1pqFVgymoPw"
+}
