20caed10ee13aa2b8538a029c7490eed49fd8be3
eunpyo hong <sinbein9@gmail.com>
2025-05-08 23:58:23 +0900

    ✨[feat]: 멤버 출석 현황 및 일정표 화면 구현 (#49)
    * 🔥[del]: 중복 .gitignore 파일 삭제 및 캐시 삭제
    
    * ✨[feat]: 멤버 - 출석 현황 화면 구현 #28
    
    * ✨[feat]: 멤버 - 출석 현황 터치 시 표시되는 Alert 컴포넌트 추가 #28
    
    * ➕[add]: 일정표 관련 이미지 에셋 추가 #30
    
    * ✨[feat]: 출석 조회와 관련된 response model 추가 #30
    
    * ✨[feat]: 일정표 목록 컴포넌트 추가 #30
    
    * ♻️[refactor]: AttendanceCard 경고 아이콘 표시 여부 로직을 컴포넌트 외부로 분리
    
    * ✨[feat]: 일정표 목록 UI 구현 #30
    
    * 🐛[fix]: 멤버 메인 화면 스크롤바 숨김 처리

--- Attendance/Projects/Presentation/Presentation/Sources/Member/Coordinator/Reducer/MemberCoordinator.swift
+++ Attendance/Projects/Presentation/Presentation/Sources/Member/Coordinator/Reducer/MemberCoordinator.swift
@@ -11,22 +11,22 @@ import Networkings
 import Utill
 
 import ComposableArchitecture
-import KeychainAccess
 import TCACoordinators
 
 @Reducer
 public struct MemberCoordinator {
   public init() {}
-  
+
   @ObservableState
   public struct State: Equatable {
+    @Shared(.appStorage("UserUID")) var userUid: String = ""
     var routes: [Route<MemberScreen.State>]
-    
+
     public init() {
       routes = [.root(.member(.init()), embedInNavigationView: true)]
     }
   }
- 
+
   public enum Action: ViewAction, BindableAction, FeatureAction {
     case binding(BindingAction<State>)
     case router(IndexedRouterActionOf<MemberScreen>)
@@ -35,87 +35,91 @@ public struct MemberCoordinator {
     case async(AsyncAction)
     case navigation(NavigationAction)
   }
-  
+
   @CasePathable
   public enum View {
-    
+
   }
-  
+
   public enum AsyncAction: Equatable {
-    
+
   }
 
   public enum InnerAction: Equatable {
-    
+
   }
-  
+
   public enum NavigationAction: Equatable {
-    
+
   }
-  
+
   public var body: some ReducerOf<Self> {
     BindingReducer()
-    
+
     Reduce { state, action in
       switch action {
       case .binding:
         return .none
-        
+
       case .router(let action):
         return handleRouterAction(state: &state, action: action)
-        
+
       case .view(let action):
         return handleViewAction(state: &state, action: action)
-        
+
       case .inner(let action):
         return handleInnerAction(state: &state, action: action)
-        
+
       case .async(let action):
         return handleAsyncAction(state: &state, action: action)
-        
+
       case .navigation(let action):
         return handleNavigationAction(state: &state, action: action)
       }
     }
+    .forEachRoute(\.routes, action: \.router)
   }
-  
+
   private func handleRouterAction(
     state: inout State,
     action: IndexedRouterActionOf<MemberScreen>
   ) -> Effect<Action> {
-//    switch action {
-//    case .routeAction(id: _, action: .)
-//      state.routes.push()
+    switch action {
+    case .routeAction(id: _, action: .member(.navigation(.routeToProfile))):
+      // TODO: - navigate to profile View
       return .none
-//    }
+
+    default:
+      return .none
+    }
   }
-  
+
   private func handleViewAction(
     state: inout State,
     action: View
   ) -> Effect<Action> {
-    return .none
+
   }
-  
+
   private func handleInnerAction(
     state: inout State,
     action: InnerAction
   ) -> Effect<Action> {
-    return .none
+
   }
-  
+
   private func handleAsyncAction(
     state: inout State,
     action: AsyncAction
   ) -> Effect<Action> {
-    return .none
+
   }
-  
+
   private func handleNavigationAction(
     state: inout State,
     action: NavigationAction
   ) -> Effect<Action> {
-    return .none
+
   }
 }
 
