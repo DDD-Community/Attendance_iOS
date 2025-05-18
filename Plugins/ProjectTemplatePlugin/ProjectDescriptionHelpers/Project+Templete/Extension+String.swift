f1d32e373348f37f632cdb9e7cec3b07e6120a1b
Roy <shuwj81@daum.net>
2025-05-19 02:22:41 +0900

    ✨[feat]: 버전 변경 및  fastalane 수정

--- Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/Extension+String.swift
+++ Plugins/ProjectTemplatePlugin/ProjectDescriptionHelpers/Project+Templete/Extension+String.swift
@@ -17,7 +17,7 @@ extension String {
     return Project.Environment.bundlePrefix
   }
   
-  public static func appBuildVersion(buildVersion: String = "24") -> String {
+  public static func appBuildVersion(buildVersion: String = "27") -> String {
     return buildVersion
   }
   
