ebf796ea6caf48f610cf031e996b2710fac763cc
Roy <shuwj81@daum.net>
2025-07-23 18:19:34 +0900

    ➕[add] : QRCod Repository 추가  #56

---
+++ Projects/Core/Data/Repository/Sources/QRCode/DefaultQRCodeRepositoryImpl.swift
@@ -0,0 +1,30 @@
+//
+//  DefaultQRCodeRepositoryImpl.swift
+//  Repository
+//
+//  Created by Wonji Suh  on 7/23/25.
+//
+
+import SwiftUI
+
+import DomainInterface
+import Model
+
+final public class DefaultQRCodeRepository: QRCodeInterface {
+  public init() {}
+
+  public func createQRCode() async throws -> String {
+    return ""
+  }
+
+  public func generateQRCode(from string: String) async -> Image? {
+    return nil
+  }
+
+  public func qrAttendanceCheck(
+    from code: String
+  ) async throws -> QRValidateModel? {
+    return nil
+  }
+}
+
