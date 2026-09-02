bfa9e4173c8599f5ab639604cdb50cf390520fc9
Roy <suhwj81@gmail.com>
2026-09-02 17:01:14 +0900

    🐛[fix]: 테스트 호스트가 SwiftUI를 먼저 적재하도록 수정
    DDDDesignKitTests 5건과 DDDCoreUITests 1건이 Crash: DDDTestHost 로
    죽었다. 호스트 소스가 import UIKit 한 줄뿐이라 SwiftUI 가 프로세스에
    올라오지 않은 상태에서 테스트 번들이 로드되며 충돌한다.
    
    매니페스트 주석은 Sharing/SwiftUI 를 먼저 로드한다고 적혀 있었지만
    의존성 선언만으로는 -dead_strip 이 걷어간다. didFinishLaunching 에서
    UIHostingController 를 실제로 만들어 링크를 붙든다.

--- Projects/TestHost/Sources/DDDTestHostAppDelegate.swift
+++ Projects/TestHost/Sources/DDDTestHostAppDelegate.swift
@@ -1,5 +1,21 @@
+import SwiftUI
 import UIKit
 
 /// Xcode 26에서 SwiftUI 의존 테스트 번들을 안전하게 로드하기 위한 최소 호스트 앱입니다.
 @main
-final class DDDTestHostAppDelegate: UIResponder, UIApplicationDelegate {}
+final class DDDTestHostAppDelegate: UIResponder, UIApplicationDelegate {
+  func application(
+    _: UIApplication,
+    didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    preloadSwiftUI()
+    return true
+  }
+
+  /// Xcode 26.3은 테스트 번들을 로드하며 SwiftUI 메타데이터를 탐색하는데,
+  /// 호스트 프로세스에 SwiftUI가 먼저 올라와 있지 않으면 그 과정에서 죽는다.
+  /// 의존성 선언만으로는 dead_strip 이 걷어가므로 실제 타입을 만들어 링크를 붙든다.
+  private func preloadSwiftUI() {
+    _ = UIHostingController(rootView: EmptyView())
+  }
+}
